6f1cd876fde9f64e67817f5e3193d467a01cbe3c
ManasaAedunuri <AEDUNURI-MANASA.AEDUNURI-MANASA@CAPGEMINI.COM>
2025-10-06 14:40:52 +0530

    file added

---
+++ EvenOddCount.java
@@ -0,0 +1,29 @@
+package arrays_package;
+
+public class EvenOddCount {
+
+	public static void main(String[] args) {
+
+		int[] arr = {1,2,3,4,5,6,7,8,9,10,11};
+		int ecount = 0;
+		int ocount = 0;
+		
+		for (int i=0; i<arr.length; i++)
+		{
+			if (arr[i] % 2 == 0) 
+			{
+				ecount++;
+			}
+			else
+			{
+				ocount++;
+			}
+		}
+		
+		System.out.print("Even numbers count : " + ecount);
+		System.out.println();
+		System.out.print("Odd numbers count : " + ocount);
+		
+	}
+
+}
